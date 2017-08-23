cd24804e531202048654ff1fd969185da005fa66
lgarzon <lgarzon48@yahoo.es>
2017-08-22 22:44:11 -0500

    He agregado un boton

--- pagina-web/Form1.cs
+++ pagina-web/Form1.cs
@@ -16,5 +16,10 @@ namespace pagina_web
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Mundo");
+        }
     }
 }
